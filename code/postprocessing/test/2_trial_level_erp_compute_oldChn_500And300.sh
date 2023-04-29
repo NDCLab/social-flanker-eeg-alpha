9419b7789a73314985e6936e13534426bbbe947c
Yanbin Niu <yn2352@tc.columbia.edu>
2023-04-29 10:55:18 -0400

    analyses
    analyses
    
    remove results
    
    Add code from YN

---
+++ code/postprocessing/test/2_trial_level_erp_compute_oldChn_500And300.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+#SBATCH --job-name=Yanbin_sfe_step2         # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1000:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=yniu@fiu.edu
+
+module load matlab-2021b
+pwd; hostname; date
+
+
+matlab -nodisplay < 2_trial_level_erp_compute_oldChn_500And300.m
